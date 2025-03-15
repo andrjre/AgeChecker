3cdd4f0b1f63adbfbc43f494340ff6de8e4711db
andrjre <andrjre@gmail.com>
2025-03-15 18:50:09 +0000

    Working on funcionality.

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+let userInput = document.getElementById("userInput");
+let userSubmit = document.getElementById("userSubmit");
+let age = userInput.value;
+
+
+userSubmit.onclick = function(){
+    console.log(age)
+}
+
+
+
