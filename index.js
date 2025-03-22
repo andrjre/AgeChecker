82fa28415c2ab6c9f0249eb8aef75121c0395739
andrjre <andrjre@gmail.com>
2025-03-22 09:31:37 +0000

    Functionality added.

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 
+// user enters age 
+// take age and return it 
+// comment on age
 
-let userInput = document.getElementById("userInput");
-let userSubmit = document.getElementById("userSubmit");
-let age = userInput.value;
+let userInput = document.getElementById("userInput")
+let userSubmit = document.getElementById("userSubmit")
+let result = document.getElementById("result")
 
 
 userSubmit.onclick = function(){
+
+    let age = userInput.value;
     console.log(age)
+
+    if(age <= 0 ){
+        result.innerHTML = `Just not possible is it?`
+    }
+    else if(age >= 18){
+        result.innerHTML = `Your age is ${age}, you can buy alcohol in the UK!`
+
+    }
+    else if (0 > age > 1){
+        result.innerHTML = `You are a mere months old!`
+    }
+    else if (age < 18){
+        result.innerHTML = `You are ${age}, not legal buddy.`
+    }
+
+
+
 }
-
-
-
